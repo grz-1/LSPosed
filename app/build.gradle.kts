0dd12fc08c9b7485b3d0005cd611fc47d4104fbc
PKQYPKJ <160383459+PKQandBaaad@users.noreply.github.com>
2025-08-22 21:06:11 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles("proguard-rules.pro")
         }
     }
