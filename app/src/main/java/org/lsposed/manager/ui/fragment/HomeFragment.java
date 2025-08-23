b689c8c6419aab514e364f2792199e6a3e824d78
PKQandBaaad <pkqypkj@outlook.com>
2025-08-23 19:53:49 +0800

    Re: WelcomeDialog

--- app/src/main/java/org/lsposed/manager/ui/fragment/HomeFragment.java
+++ app/src/main/java/org/lsposed/manager/ui/fragment/HomeFragment.java
@@ -48,6 +48,7 @@ import org.lsposed.manager.databinding.DialogAboutBinding;
 import org.lsposed.manager.databinding.FragmentHomeBinding;
 import org.lsposed.manager.ui.dialog.BlurBehindDialogBuilder;
 import org.lsposed.manager.ui.dialog.FlashDialogBuilder;
+import org.lsposed.manager.ui.dialog.WelcomeDialog;
 import org.lsposed.manager.util.NavUtil;
 import org.lsposed.manager.util.Telemetry;
 import org.lsposed.manager.util.UpdateUtil;
@@ -69,6 +70,7 @@ public class HomeFragment extends BaseFragment implements MenuProvider {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WelcomeDialog.showIfNeed(getChildFragmentManager());
     }
 
     @Override
